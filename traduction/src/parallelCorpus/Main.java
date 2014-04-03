0dbfc713cad3e90bef88e6c23e3815b7f84c8a09
fenix01 <p-yohann@hotmail.fr>
2014-04-03 12:15:59 +0200

    affichage de l'aer dans la console

--- traduction/src/parallelCorpus/Main.java
+++ traduction/src/parallelCorpus/Main.java
@@ -1,5 +1,9 @@
 package parallelCorpus;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -12,8 +16,8 @@ public class Main {
 		bi_fr_en.parseFile();
 		bi_en_fr.parseFile();
 		long start = System.currentTimeMillis();
-		Accumulation acc_fr_en = new Accumulation(bi_fr_en,500,50);
-		Accumulation acc_en_fr = new Accumulation(bi_en_fr,500,50);
+		Accumulation acc_fr_en = new Accumulation(bi_fr_en,200,50);
+		Accumulation acc_en_fr = new Accumulation(bi_en_fr,200,50);
 		acc_fr_en.compute();
 		acc_en_fr.compute();
 		Symmetrize sym= new Symmetrize(acc_fr_en.getAccAlignments(), acc_en_fr.getAccAlignments());
@@ -23,6 +27,29 @@ public class Main {
 		long end = System.currentTimeMillis();
 		float time = ((float) (end-start)) / 1000f;
 		System.out.println(time);
+		
+		try {
+			Process process = Runtime
+					.getRuntime()
+					.exec("perl aa_eval_align.pl reference_alignments.txt my_alignments2.txt");
+			process.waitFor();
+			if (process.exitValue() == 0) {
+				try {
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(process.getInputStream()));
+					String line = null;
+					while ((line = in.readLine()) != null) {
+						System.out.println(line);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("Command Failure");
+			}
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.toString());
+		}
 	}
 
 }
